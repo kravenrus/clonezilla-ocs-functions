c2437b585644521c8ddd29e1ab5b02ff9a06aa8a
kravenrus <47505285+kravenrus@users.noreply.github.com>
2024-09-27 00:19:05 +0400

    Update change.clonezilla-live-amd64-3.1.3-16-and-20240715-noble.ocs-functions.sh

--- change.clonezilla-live-amd64-3.1.3-16-and-20240715-noble.ocs-functions.sh
+++ change.clonezilla-live-amd64-3.1.3-16-and-20240715-noble.ocs-functions.sh
@@ -4,6 +4,9 @@
 # apt update && apt install -y open-vm-tools-desktop git squashfs-tools
 # cd /tmp
 # git clone https://github.com/kravenrus/clonezilla-ocs-functions.git
+# cd clonezilla-ocs-functions
+# chmod +x change.clonezilla-live-amd64-3.1.3-16-and-20240715-noble.ocs-functions.sh
+# ./change.clonezilla-live-amd64-3.1.3-16-and-20240715-noble.ocs-functions.sh
 
 # Создание рабочего каталога
 mkdir -p /home/ubuntu/clonezilla/custom; cd /home/ubuntu/clonezillals
@@ -36,11 +39,11 @@ fi
 # Распаковка образа
 unsquashfs filesystem.squashfs
 # Внесение изменений в скрипт ../squashfs-root/usr/share/drbl/sbin/ocs-functions
-sed -i '/    initrd_tool="update-initramfs"/a \  elif [ -e "$mnt_pnt/$extra_root_path/usr/sbin/make-initrd" ]; then\n    initrd_tool="make-initrd"' /home/user/clonezilla/squashfs-root/usr/share/drbl/sbin/ocs-functions
-sed -i '/  case "$initrd_tool" in/a \    # initrd_tool for ALT Education 10.2 or earlier x86_64 (from 26.09.2024 by Kravenrus)\n    "make-initrd")\n      mkinitrd_cmd="make-initrd --kernel=$(ls $mnt_pnt/$extra_root_path/lib/modules | grep alt)" ;;' /home/user/clonezilla/squashfs-root/usr/share/drbl/sbin/ocs-functions
+sed -i '/    initrd_tool="update-initramfs"/a \  elif [ -e "$mnt_pnt/$extra_root_path/usr/sbin/make-initrd" ]; then\n    initrd_tool="make-initrd"' /home/ubuntu/clonezilla/squashfs-root/usr/share/drbl/sbin/ocs-functions
+sed -i '/  case "$initrd_tool" in/a \    # initrd_tool for ALT Education 10.2 or earlier x86_64 (from 26.09.2024 by Kravenrus)\n    "make-initrd")\n      mkinitrd_cmd="make-initrd --kernel=$(ls $mnt_pnt/$extra_root_path/lib/modules | grep alt)" ;;' /home/ubuntu/clonezilla/squashfs-root/usr/share/drbl/sbin/ocs-functions
 # Запаковка образа
 mksquashfs squashfs-root/ custom/filesystem.squashfs
 # Ожидание нажатия любой клавиши для продолжения
 echo Сохраните копию Squashfs-образа в папке custom и нажмите любую клавишу для продолжения...; read -n 1
 # Удаление рабочего каталога
-rm -R /home/user/clonezilla
+rm -R /home/ubuntu/clonezilla
